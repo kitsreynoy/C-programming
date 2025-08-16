994bf16ab3cf23ea35afaa0fb28dc8d4a25e5aad
kitsreynoy <kitsreynoykh@gmail.com>
2025-08-16 21:43:27 +0700

    Find the area of rectangle

---
+++ find calculate/Calculate the Area of a Rectangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int length = 7;
+	int width = 5;
+	
+	int area= length*width;
+	
+	cout<<"input a length: "<<length<<"\n";
+	cout<<"input a width: "<<width<<"\n";
+	cout<<"The result of area is :"<<area<<"\n";
+	return 0;
+}

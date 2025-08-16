bcb0566f84017673d3c0828d1ce2efaa57a074f9
kitsreynoy <kitsreynoykh@gmail.com>
2025-08-16 22:20:49 +0700

    This is c++ demo

---
+++ C++ Demo/First start c++.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	system("cls");
+	cout << "Welcome to coding" <<endl;
+	cout << "Hello and welcoming to c++ programming. "<< endl;
+	return 0;
+	
+}
